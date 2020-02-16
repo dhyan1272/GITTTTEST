0bcabe9d3facb6fa6f06adb33518535b617b895c
Dhyanjyoti <nathd@rpi.edu>
2020-02-16 17:28:20 -0500

    X

--- main_1.cpp
+++ main_1.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-	cout<<"Hello World\n";
+	cout<<"Hello World from the 2nd person\n";
 	cout<<"Added a new line in the desktop that is not in github\n";
 
 }
